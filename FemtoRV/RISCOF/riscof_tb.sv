01883929507d69b07f8839b6b46f3ff1ee7723fd
Iztok Jeras <iztok.jeras@gmail.com>
2025-12-24 00:01:55 +0100

    initial RISCOF testbench for 'quark'

---
+++ FemtoRV/RISCOF/riscof_tb.sv
@@ -0,0 +1,201 @@
+////////////////////////////////////////////////////////////////////////////////
+// FemtoRV RISCOF testbench
+// FemtoRV32 as DUT
+////////////////////////////////////////////////////////////////////////////////
+// Copyright 2025 Iztok Jeras
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+///////////////////////////////////////////////////////////////////////////////
+
+module riscof_tb #(
+    // memory parameters
+    parameter int unsigned MEM_BASE = 32'h8000_0000,
+    parameter int unsigned MEM_SIZE = 32'h0020_0000,
+    // global constants
+    localparam int unsigned XLEN = 32
+)();
+
+    // runtime arguments
+    string       test_dir;
+    int unsigned tohost;
+    int unsigned fromhost;
+    int unsigned begin_signature;
+    int unsigned end_signature;
+
+    // system signals
+    logic clk = 1'b1;  // clock
+    logic rst = 1'b1;  // reset
+
+    // system bus
+    logic [XLEN  -1:0]        mem_addr ;  // address bus
+    logic [XLEN/8-1:0][8-1:0] mem_wdata;  // data to be written
+    logic [XLEN/8-1:0]        mem_wmask;  // write mask for the 4 bytes of each word
+    logic [XLEN/8-1:0][8-1:0] mem_rdata;  // input lines for both data and instr
+    logic                     mem_rstrb;  // active to initiate memory read (used by IO)
+    logic                     mem_rbusy;  // asserted if memory is busy reading value
+    logic                     mem_wbusy;  // asserted if memory is busy writing value
+
+    // memory array
+    logic [8-1:0] memory_array [0:MEM_SIZE-1];
+
+    ////////////////////////////////////////////////////////////////////////////////
+    // RTL DUT instance
+    ////////////////////////////////////////////////////////////////////////////////
+
+    FemtoRV32 #(
+      .RESET_ADDR (32'h8000_0000),
+      .ADDR_WIDTH (XLEN)
+    ) DUT (
+      .clk       ( clk),
+      .reset     (~rst),       // set to 0 to reset the processor
+      .mem_addr  (mem_addr ),  // address bus
+      .mem_wdata (mem_wdata),  // data to be written
+      .mem_wmask (mem_wmask),  // write mask for the 4 bytes of each word
+      .mem_rdata (mem_rdata),  // input lines for both data and instr
+      .mem_rstrb (mem_rstrb),  // active to initiate memory read (used by IO)
+      .mem_rbusy (mem_rbusy),  // asserted if memory is busy reading value
+      .mem_wbusy (mem_wbusy)   // asserted if memory is busy writing value
+    );
+
+    ////////////////////////////////////////////////////////////////////////////////
+    // memory
+    ////////////////////////////////////////////////////////////////////////////////
+
+    // memory array
+    always_ff @(posedge clk)
+    begin
+        // write access
+        for (int unsigned i=0; i<XLEN/8; i++) begin
+            if (mem_wmask[i]) memory_array[mem_addr+i] <= mem_wdata[i];
+        end
+        // read access
+        if (mem_rstrb) begin
+            for (int unsigned i=0; i<XLEN/8; i++) begin
+                mem_rdata[i] <= memory_array[mem_addr+i];
+            end
+        end
+    end
+
+    // memory control signals
+    always_ff @(posedge rst, posedge clk)
+    begin
+        if (rst) begin
+            mem_wbusy <= 1'b1;
+            mem_rbusy <= 1'b1;
+        end else begin
+            // write access
+            mem_wbusy <= 1'b0;
+            // read access
+            mem_rbusy <= ~mem_rstrb;
+        end
+    end
+
+    // memory initialization
+    function int memory_init_bin (
+        string filename,
+        int unsigned mem_begin = 0
+    );
+        int code;  // status code
+        int fd;    // file descriptor
+        bit [640-1:0] err;
+        fd = $fopen(filename, "rb");
+        code = $fread(memory_array, fd);
+`ifndef VERILATOR
+        if (code == 0) begin
+            code = $ferror(fd, err);
+            $display("DEBUG: read_bin: code = %d, err = %s", code, err);
+        end else begin
+            $display("DEBUG: read %dB from binary file", code);
+        end
+`endif
+        $fclose(fd);
+        return code;
+    endfunction: memory_init_bin
+
+    // memory initialization
+    function int unsigned memory_dump_hex (
+        string filename,
+        int unsigned mem_begin = 0,
+        int unsigned mem_end = 0
+    );
+        int fd;    // file descriptor
+        fd = $fopen(filename, "w");
+        // check if file was opened successfully
+        if (fd == 0) return 0;
+        for (int unsigned addr=mem_begin; addr<mem_end; addr+=XLEN/8) begin
+            for (int unsigned i=0; i<XLEN/8; i++) begin
+                $fwrite(fd, "%02h", memory_array[addr+i]);
+            end
+            $fwrite(fd, "\n");
+
+        end
+        $fclose(fd);
+        return mem_end-mem_begin;
+    endfunction: memory_dump_hex
+
+    ////////////////////////////////////////////////////////////////////////////////
+    // controller
+    ////////////////////////////////////////////////////////////////////////////////
+
+    // clock
+    always #(20ns/2) clk = ~clk;
+
+    /* verilator lint_off INITIALDLY */
+    initial
+    begin
+        string firmware, signature;
+        logic htif_halt = 1'b0;
+        // get/display test directory from plusargs
+        assert ($value$plusargs("test_dir=%s"       , test_dir       ))  $display("RISCOF: test_dir = \'%s\'", test_dir);  else  $fatal(0, "RISCOF: test_dir $plusarg not found!");
+        // get/display ELF symbols from plusargs
+        assert ($value$plusargs("begin_signature=%h", begin_signature))  $display("HTIF: begin_signature = 0x%08h", begin_signature);  else  $fatal(0, "HTIF: begin_signature $plusarg not found!");
+        assert ($value$plusargs("end_signature=%h"  , end_signature  ))  $display("HTIF: end_signature   = 0x%08h", end_signature  );  else  $fatal(0, "HTIF: end_signature   $plusarg not found!");
+        assert ($value$plusargs("tohost=%h"         , tohost         ))  $display("HTIF: tohost          = 0x%08h", tohost         );  else  $fatal(0, "HTIF: tohost          $plusarg not found!");
+        assert ($value$plusargs("fromhost=%h"       , fromhost       ))  $display("HTIF: fromhost        = 0x%08h", fromhost       );  else  $fatal(0, "HTIF: fromhost        $plusarg not found!");
+        // load binary file into memory
+        firmware = {test_dir, "DUT.bin"};
+        $display("Loading file into memory: %s", firmware);
+        assert (memory_init_bin(firmware) > 0) begin
+            $display("RISCOF: loaded firmware file \'%s\' into memory.", firmware);
+        end else begin
+            $fatal  ("RISCOF: firmware file \'%s\' not found.", firmware);
+        end
+        // RESET sequence
+        repeat (4) @(posedge clk);
+        rst <= 1'b0;
+        // wait for HTIF halt request
+        signature = {test_dir, "signature.hex"};
+        do begin
+            @(posedge clk);
+            if (&mem_wmask && (mem_addr == 32'h0000_0010)) begin
+                htif_halt <= mem_wdata[0][0];
+            end
+            if (htif_halt) begin
+                assert (memory_dump_hex(signature, begin_signature-MEM_BASE, end_signature-MEM_BASE) > 0) begin
+                    $display("RISCOF: saved signature from 0x%8h to 0x%8h into file \'%s\'", begin_signature, end_signature, signature);
+                end else begin
+                    $fatal  ("RISCOF: could not save signature file \'%s\'.", signature);
+                end
+            end
+        end while (!htif_halt);
+        // wait a few clock cycles before finishing simulation
+        repeat(4) @(posedge clk);
+        $finish;
+    end
+    /* verilator lint_on INITIALDLY */
+
+    ////////////////////////////////////////////////////////////////////////////////
+    // Verbose execution trace
+    ////////////////////////////////////////////////////////////////////////////////
+
+endmodule: riscof_tb
---
+++ FemtoRV/RISCOF/spike/riscof_spike.py
@@ -0,0 +1,148 @@
+import os
+import re
+import shutil
+import subprocess
+import shlex
+import logging
+import random
+import string
+from string import Template
+
+import riscof.utils as utils
+import riscof.constants as constants
+from riscof.pluginTemplate import pluginTemplate
+
+logger = logging.getLogger()
+
+class spike(pluginTemplate):
+    __model__ = "spike"
+    __version__ = "0.1.0"
+
+    def __init__(self, *args, **kwargs):
+        """
+        Process config.ini file and setup logging.
+        """
+        sclass = super().__init__(*args, **kwargs)
+
+        config = kwargs.get('config')
+        if config is None:
+            logger.error(self.name + " config node is missing.")
+            raise SystemExit(1)
+        self.num_jobs = str(config['jobs'] if 'jobs' in config else 1)
+        self.pluginpath = os.path.abspath(config['pluginpath'])
+        self.isa_spec = os.path.abspath(config['ispec']) if 'ispec' in config else ''
+        self.platform_spec = os.path.abspath(config['pspec']) if 'ispec' in config else ''
+        self.make = config['make'] if 'make' in config else 'make'
+        logger.debug(self.name + " plugin initialized using the following configuration:")
+        self.config = config
+        for entry in config:
+            logger.debug(entry+' : '+config[entry])
+        return sclass
+
+    def initialise(self, suite, work_dir, archtest_env):
+        """
+        Prepare toolchain, and model executables.
+        Checking whether the executables are available is done in the build step,
+        since this step lacks XLEN information from YAML files.
+        """
+        self.suite = suite
+        self.work_dir = work_dir
+
+        # NOTE: The following assumes you are using the riscv-gcc toolchain.
+        #       If not please change appropriately.
+        # prepare toolchain executables ({} to be replaced with XLEN)
+        self.objdump_exe = 'riscv{}-unknown-elf-objdump'
+        self.compile_exe = 'riscv{}-unknown-elf-gcc'
+
+        # prepare toolchain command template
+        self.objdump_cmd = self.objdump_exe + ' -M no-aliases -M numeric -D {} > {}'
+        self.compile_cmd = self.compile_exe + (
+            ' -march={}'
+            ' -static -mcmodel=medany -fvisibility=hidden -nostdlib -nostartfiles'
+            ' -T ' + self.pluginpath + '/env/link.ld'
+            ' -I ' + self.pluginpath + '/env/'
+            ' -I ' + archtest_env
+        )
+
+        # prepare model executable
+        self.ref_exe = os.path.join(self.config['PATH'] if 'PATH' in self.config else "","spike")
+
+    def build(self, isa_yaml, platform_yaml):
+        """
+        Build is run only once before running for each test list.
+        """
+        # load ISA YAML file
+        ispec = utils.load_yaml(isa_yaml)['hart0']
+
+        self.xlen = ('64' if 64 in ispec['supported_xlen'] else '32')
+        # construct ISA string
+        self.isa = 'rv' + self.xlen
+        for ext in ['I', 'M', 'C', 'F', 'D']:
+            if ext in ispec["ISA"]:
+                self.isa += ext.lower()
+
+        # NOTE: The following assumes you are using the riscv-gcc toolchain.
+        #       If not please change appropriately.
+        # extend compiler command with ABI (integer and floating-point calling convention)
+        self.compile_cmd = self.compile_cmd+' -mabi='+('lp64 ' if 64 in ispec['supported_xlen'] else 'ilp32 ')
+
+        # check if toolchain executables are available
+        for tool in [self.compile_exe, self.objdump_exe]:
+            tool_xlen = tool.format(self.xlen)
+            if shutil.which(tool_xlen) is None:
+                logger.error(tool_xlen + ": executable not found. Please check environment setup.")
+                raise SystemExit(1)
+            
+        # check if the reference executable is available
+        if shutil.which(self.ref_exe.format(self.xlen)) is None:
+            logger.error(self.ref_exe.format(self.xlen) + ": executable not found. Please check environment setup.")
+            raise SystemExit(1)
+        
+        # check if 'make' is available
+        if shutil.which(self.make) is None:
+            logger.error(self.make + ": executable not found. Please check environment setup.")
+            raise SystemExit(1)
+
+    def runTests(self, testList, cgf_file=None):
+        # remove ':' from the end of the name
+        name = self.name[:-1]
+        if os.path.exists(self.work_dir+ "/Makefile." + name):
+            os.remove(self.work_dir+ "/Makefile." + name)
+        make = utils.makeUtil(makefilePath=os.path.join(self.work_dir, "Makefile." + name))
+        make.makeCommand = self.make + ' -j' + self.num_jobs
+        for file in testList:
+            testentry = testList[file]
+            test = testentry['test_path']
+            test_dir = testentry['work_dir']
+            test_name = test.rsplit('/',1)[1][:-2]
+
+            elf = 'ref.elf'
+
+            execute = ''
+
+            # prepare list of commands to execute
+            cmd = "@cd " + testentry['work_dir']
+            execute += f'{cmd};\\\n'
+
+            # NOTE: The following assumes you are using the riscv-gcc toolchain.
+            #       If not please change appropriately.
+            # compile testcase assembly into an elf file
+            cmd = self.compile_cmd.format(self.xlen, testentry['isa'].lower()) + (
+                f' {test}'
+                f' -o {elf}'
+                f' -D{" -D".join(testentry['macros'])}'
+            )
+            execute += f'{cmd};\\\n'
+
+            # dump disassembled elf file
+            cmd = self.objdump_cmd.format(self.xlen, elf, 'ref.disass')
+            execute += f'{cmd};\\\n'
+
+            # run reference model
+            # signature-granularity specifies how many bytes in HEX are in a line of the signature file
+            cmd = self.ref_exe + f' --isa={self.isa} --log-commits +signature={name}.signature +signature-granularity=4 {elf} > ref.log 2>&1'
+            execute += f'{cmd};\\\n'
+
+
+            make.add_target(execute)
+        make.execute_all(self.work_dir)
